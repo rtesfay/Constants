db00e8f95093bf770ef3310537556d3ad9fb0653
rteklu <rahelteklu@gmail.com>
2021-10-04 04:17:43 -0500

    Initial commit

---
+++ src/Constants.java
@@ -0,0 +1,15 @@
+public class Constants {
+        public static void main(String[] args) {
+            final static double PI=3.14;
+            final static int MONTH_OF_THE_YEAR=12;
+            double radius, area,circumference;
+            //Compute area and circumference;
+            area=PI*radius*radius;
+            circumference=PI*2*radius;
+            System.out.println("Given radius "+radius);
+            System.out.println("Given circumference "+circumference);
+            System.out.println("Given area "+area);
+        }
+    }
+
+
